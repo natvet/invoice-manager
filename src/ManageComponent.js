62f4c7baadbade1433b15bf348e849268ed7446d
Natalia Vetter <nataliavetter@gmail.com>
2018-01-07 12:06:43 +0100

    Enable displaying invoice details

--- src/ManageComponent.js
+++ src/ManageComponent.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import { Header, Segment, Checkbox, Grid } from 'semantic-ui-react';
+import { Header, Segment, Checkbox, Grid, Icon, List } from 'semantic-ui-react';
 import moment from 'moment';
+import './ManageComponent.css';
 
 class ManageComponent extends Component {
+  state={}
+
+  handleToggleDetails = (index) => {
+    if(this.state[index] === 'open') {
+      this.setState({[index]: 'hide'})
+    } else {
+      this.setState({[index]: 'open'})
+    }
+  }
 
   render() {
     return (
-      <div>
+      <div className='c-ManageComponent'>
         <h3>Manage Invoices</h3>
-        {!this.props.invoices && <Header>You don't have any invoices yet</Header>}
-        {this.props.invoices &&
+        {this.props.invoices.length === 0 && <Header>You don't have any invoices yet</Header>}
+        {this.props.invoices.length !== 0 &&
         <Segment.Group>
-          <Segment>
+          <Segment className='c-ManageComponent__head'>
+          {console.log(this.props.invoices)}
             <Grid>
               <Grid.Column width={1}>
                 <Checkbox/>
               </Grid.Column>
               <Grid.Column width={3}>
-                <b>Invoice Number</b>
+                Invoice Number
               </Grid.Column>
               <Grid.Column width={3}>
-                <b>Customer Number</b>
+                Customer Number
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Invoice Date
               </Grid.Column>
               <Grid.Column width={3}>
-                <b>Invoice Date</b>
+                Invoice Amount
               </Grid.Column>
-              <Grid.Column width={3}>
-                <b>Invoice Amount</b>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <b>Due Date</b>
+              <Grid.Column width={2}>
+                Due Date
               </Grid.Column>
             </Grid>
           </Segment>
@@ -45,16 +56,30 @@ class ManageComponent extends Component {
                 <Grid.Column width={3}>
                   {invoice.customerNumber}
                 </Grid.Column>
-                <Grid.Column width={3}>
+                <Grid.Column width={2}>
                   {moment(invoice.invoiceDate).format('L')}
                 </Grid.Column>
                 <Grid.Column width={3}>
-                  {invoice.invoiceAmount}
+                  ${invoice.invoiceAmount}
                 </Grid.Column>
-                <Grid.Column width={3}>
+                <Grid.Column width={2}>
                   {moment(invoice.dueDate).format('L')}
                 </Grid.Column>
+                <Grid.Column width={2} className='c-ManageComponent__show-more' onClick={this.handleToggleDetails.bind(this, index)}>
+                  {this.state[index] === 'open' ? 'Hide' : 'Show'} Details
+                  <Icon name={this.state[index] === 'open' ? 'chevron up' : 'chevron down'}/>
+                </Grid.Column>
               </Grid>
+              {this.state[index] === 'open' &&
+              <Segment>
+                <div><b>Products: </b></div>
+                <List horizontal divided>
+                  {invoice.products.map((item, i) => <List.Item key={i}>{item.name}: ${item.cost}</List.Item>)}
+                </List>
+                <div><b>Customer Country:</b> {invoice.customerCountry}</div>
+                <div><b>Attached file:</b> {invoice.file}</div>
+              </Segment>
+              }
             </Segment>
           ))}
         </Segment.Group>
